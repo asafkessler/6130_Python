dee8f14f3556055f6545d30a21c526ae3138e067
Noam yam  amitai <noamyamamity@gmail.com>
2023-05-30 15:41:10 +0300

    asdfg

--- course_6130/EX5/ex5b.py
+++ course_6130/EX5/ex5b.py
@@ -1,6 +1,9 @@
 # 123456789, 123456789
 # time
 
+import numpy
+import pandas
+
 # question 2
 def create_season_rating(csv_folder):
     pass
@@ -11,3 +14,8 @@ def most_lines_in_the_second_most_popular_episode(csv_folder):
 
 def who_said_it(csv_folder, character_name, phrase):
     pass
+
+
+if __name__ == "__main__":
+    path =  "C:\לימודים תשפג\סמסטר ב\פייתון 6130\תרגיל 5\data"
+    data_frame_rr = create_season_rating(path)
