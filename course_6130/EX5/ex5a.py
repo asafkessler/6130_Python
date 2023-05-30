a7a9ef3d8b27b8e50751327348dbdf2ad04e81ac
Asaf Kessler <asafkesslerofficial@gmail.com>
2023-05-30 17:57:10 +0300

    21/05/23 AsafKessler sdghj

--- course_6130/EX5/ex5a.py
+++ course_6130/EX5/ex5a.py
@@ -1,19 +1,13 @@
 # 316539196, 315535518
 # 5
+import copy
 
 import numpy as np
 import pandas as pd
 
 # Part A
+
 # 1
-x = pd.DataFrame(data={'col1': [1, 2, 3, 4, 5], 'col2': [3, 4, 5, 6, 7]})
-# col_series = x['col1']
-# filtered_indexes = col_series[(col_series >= 2) & (col_series < 4)]
-# new_df = x[list(filtered_indexes.index)[0]: list(filtered_indexes.index)[1] + 1:]
-x._set_axis_name(name="asaf", axis=1)
-print(list(x.columns))
-
-
 def filter_numeric_column(df, col_name, num1, num2):
     col_series = df[col_name]
     filtered_indexes = col_series[(col_series >= num1) & (col_series < num2)]
@@ -24,28 +18,66 @@ def filter_numeric_column(df, col_name, num1, num2):
 
 # 2
 def switch_col_names(df, col_name1, col_name2):
-    # col_new_list = list(col_name2,col_name1)
-    # curr_col = df.columns
-    # df_new = df.rename(columns ={curr_col[0]:col_new_list[0],curr_col[1]:col_new_list[1]})
+    """
 
-    new_df = df.rename(columns=({col_name2:col_name2,col_name2:col_name1 }))
+    :param df:
+    :type df:
+    :param col_name1:
+    :type col_name1:
+    :param col_name2:
+    :type col_name2:
+    :return:
+    :rtype:
+    """
+    new_df = df.rename(columns=({col_name2:col_name2,col_name2:col_name1}))
     return new_df
+
+def switch_col_names(df, col_name1, col_name2):
+    col_new_list = list(col_name2,col_name1)
+    curr_col = df.columns
+    df_new = df.rename(columns ={curr_col[0]:col_new_list[0],curr_col[1]:col_new_list[1]})
+    pass
 
 
 # 3
 def double_special_letters(df, col_name_letter, col_name_value, col_name_results):
-    letters_to_double = set('h','u','j','i')
-    col_results_series = df[col_name_value]
-    where_to_double = df[col_name_letter] in letters_to_double
+    """
 
-    for index in range(len(where_to_double.index)):
-            if where_to_double[index]:
-                col_results_series[index] *= 2
+    :param df:
+    :type df:
+    :param col_name_letter:
+    :type col_name_letter:
+    :param col_name_value:
+    :type col_name_value:
+    :param col_name_results:
+    :type col_name_results:
+    :return:
+    :rtype:
+    """
+
+    """
+        
+        double_special_letters(df, 'letter','value','col_res')
+    """
+    df = pd.DataFrame(data={'letter': ['h', 'a', 'b', 'c', 'i'], 'value': [3, 4, 5, 6, 7]})
+
+    letters_to_double = ['h','u','j','i']
+    col_results_series = copy.deepcopy(df[col_name_value])
+
+    where_to_double =  []
+    for curr_letter in df['letter']:
+        if curr_letter in letters_to_double:
+            where_to_double.append(True)
+        else:
+            where_to_double.append(False)
+
+    for index in range(len(where_to_double)):
+        if where_to_double[index]:
+            col_results_series[index] *= 2
 
     new_dataframe = df
     new_dataframe[col_name_results] = col_results_series
     return new_dataframe
-
 
 
 # 4
@@ -64,4 +96,14 @@ def put_row_items_together(df, col_name_results):
     # output
     # return new_df
 
-print(x.columns)
+
+if __name__ == "__main__":
+    '''
+        x = pd.DataFrame(data={'col1': [1, 2, 3, 4, 5], 'col2': [3, 4, 5, 6, 7]})
+        col_series = x['col1']
+        filtered_indexes = col_series[(col_series >= 2) & (col_series < 4)]
+        new_df = x[list(filtered_indexes.index)[0]: list(filtered_indexes.index)[1] + 1:]
+    '''
+
+    final = double_special_letters({},"letter" ,"value", "result")
+    print(final)
--- course_6130/EX5/ex5b.py
+++ course_6130/EX5/ex5b.py
@@ -1,21 +1,47 @@
 # 123456789, 123456789
 # time
+import glob
 
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
+import os
+
+PATH = r'C:\Users\asafk\Desktop\Hebrow Univarcity\First Year\Semester B\Python Cognitive Science\EX5\friends_datasets_ex5'
+
+def get_file_name_list_in_folder(folder_path):
+    files_names = glob.glob(folder_path + r'\friends_imdb_episode_season_*')
+    return list(files_names)
+
 
 # question 2
 def create_season_rating(csv_folder):
-    pass
 
+    list_files_names = get_file_name_list_in_folder(csv_folder)
+    df_all_episodes = pd.read_csv(list_files_names[0])
+
+    for index in range(1, len(list_files_names)):
+        curr_season_df = pd.read_csv(list_files_names[index])
+        df_all_episodes = pd.concat([df_all_episodes, curr_season_df], ignore_index=True, axis=0)
+
+    df_imdb = pd.read_csv(csv_folder + r'\rating_all_imdb.csv')
+    df_merged = df_all_episodes.merge(df_imdb, how="inner", on=["tconst"])
+    print(list(df_merged.shape))
+    print(list(df_merged.columns))
+
+    seasons_sum = df_merged.groupby(by="seasonNumber", as_index=False).sum()[['seasonNumber', 'averageRating', 'numVotes']]
+    seasons_rating = df_merged.groupby(by="seasonNumber", as_index=False).mean()[['seasonNumber', 'averageRating']]
+
+    final_df = seasons_rating
+    final_df["numVotes"] = seasons_sum["numVotes"]
+
+    return (final_df)
 
 def most_lines_in_the_second_most_popular_episode(csv_folder):
-    pass
+    df_line = pd.read_csv(csv_folder + r'friends_quotes.csv')
 
 def who_said_it(csv_folder, character_name, phrase):
     pass
 
 
 if __name__ == "__main__":
-    path =  "C:\לימודים תשפג\סמסטר ב\פייתון 6130\תרגיל 5\data"
-    data_frame_rr = create_season_rating(path)
+    create_season_rating(PATH)
