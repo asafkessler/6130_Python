378a8a25d237f608c734f44052c727b9d358cbf9
Asaf Kessler <asafkesslerofficial@gmail.com>
2023-06-04 20:39:32 +0300

    finishing up EX5

--- course_6130/EX5/ex5a.py
+++ course_6130/EX5/ex5a.py
@@ -1,109 +1,111 @@
 # 316539196, 315535518
-# 5
-import copy
+# 10
 
-import numpy as np
 import pandas as pd
 
-# Part A
+def check_valid_input(list_df_columns, list_user_columns):
+    """
+    Args:
+        list_df_columns (): the dataframes columns
+        list_user_columns (): the user columns
 
+    Returns:
+        checks in the data is correct.
+    """
+    return all(elem in list_df_columns for elem in list_user_columns)
+
+
+# Part A
 # 1
 def filter_numeric_column(df, col_name, num1, num2):
-    col_series = df[col_name]
-    filtered_indexes = col_series[(col_series >= num1) & (col_series < num2)]
+    """
+    Args:
+        df (): DataFrame all numeric elements?
+        col_name (): The name of a Column in df.
+        num1 (): numeric param
+        num2 (): numeric Param
+
+    Returns:
+    A DataFrame which contains only rows in which the value in the
+    given column name is between the numeric values given (including the lower value and not including the higher value).
+    """
+    column_data = df[col_name]
+
+    # finding the exact min/max params
+    (numeric_min, numeric_max) = (min(num1, num2), max(num1, num2))
+
+    # getting only the indexes of the relevant values
+    filtered_indexes = column_data[(column_data >= numeric_min) & (column_data < numeric_max)]
     index_list = list(filtered_indexes.index)
-    new_dataframe = df[index_list[0]: index_list[1] + 1:]
+
+    # slicing the data frame by the relevant indexes chosen above
+    start, end = index_list[0], index_list[0]
+    if (len(index_list) > 1):
+        end = index_list[len(index_list) - 1]
+
+    new_dataframe = df[start: end + 1: ]
+
     return new_dataframe
 
 
 # 2
 def switch_col_names(df, col_name1, col_name2):
     """
+    Args:
+        df (): DataFrame
+        col_name1 (): first name of a column
+        col_name2 (): second name of a column
 
-    :param df:
-    :type df:
-    :param col_name1:
-    :type col_name1:
-    :param col_name2:
-    :type col_name2:
-    :return:
-    :rtype:
+    Returns:
+        The safe DataFrame but with switched columns.
     """
-    new_df = df.rename(columns=({col_name2:col_name2,col_name2:col_name1}))
-    return new_df
 
-def switch_col_names(df, col_name1, col_name2):
-    col_new_list = list(col_name2,col_name1)
-    curr_col = df.columns
-    df_new = df.rename(columns ={curr_col[0]:col_new_list[0],curr_col[1]:col_new_list[1]})
-    pass
+    col_new_list = list([col_name2,col_name1])
+    curr_col = list(df.columns)
+
+    if check_valid_input(curr_col, col_new_list):
+        index_first = curr_col.index(col_name1)
+        index_second = curr_col.index(col_name2)
+        switched_df = df.rename(columns={curr_col[index_first]: col_new_list[0], curr_col[index_second]: col_new_list[1]})
+    else:
+        raise ValueError("The columns names are incorrect. Not Found in DataFrame")
+
+    return switched_df
 
 
 # 3
 def double_special_letters(df, col_name_letter, col_name_value, col_name_results):
     """
+    Args:
+        df (): general dataframe
+        col_name_letter (): column name of the letters data
+        col_name_value (): column name of the values data
+        col_name_results (): new column name of the results
 
-    :param df:
-    :type df:
-    :param col_name_letter:
-    :type col_name_letter:
-    :param col_name_value:
-    :type col_name_value:
-    :param col_name_results:
-    :type col_name_results:
-    :return:
-    :rtype:
+    Returns:
+        The function creates a new column named col_name_results
+        that contains similar values to the values column
+        The only difference is that in the new column the values are doubled if the letter is part of "huji".
     """
 
-    """
-        
-        double_special_letters(df, 'letter','value','col_res')
-    """
-    df = pd.DataFrame(data={'letter': ['h', 'a', 'b', 'c', 'i'], 'value': [3, 4, 5, 6, 7]})
+    letters_to_double = ['h', 'u', 'j', 'i']
+    if check_valid_input(list(df.columns), list([col_name_letter, col_name_value])):
+        df[col_name_results] = df.apply(lambda r: r[col_name_value]*2 if r[col_name_letter] in letters_to_double else r[col_name_value], axis=1)
+    else:
+        raise ValueError("The columns names are incorrect. Not Found in DataFrame")
 
-    letters_to_double = ['h','u','j','i']
-    col_results_series = copy.deepcopy(df[col_name_value])
-
-    where_to_double =  []
-    for curr_letter in df['letter']:
-        if curr_letter in letters_to_double:
-            where_to_double.append(True)
-        else:
-            where_to_double.append(False)
-
-    for index in range(len(where_to_double)):
-        if where_to_double[index]:
-            col_results_series[index] *= 2
-
-    new_dataframe = df
-    new_dataframe[col_name_results] = col_results_series
-    return new_dataframe
-
+    return df
 
 # 4
 def put_row_items_together(df, col_name_results):
-    pass
-    # # Create an empty list
-    # Row_list = []
-    #
-    # #Iterate over each row
-    # for rows in df.iterrows
-    #     # Create list for the current row
-    #     my_list = [rows., rows.Event, rows.Cost]
-    #     # place it in the data frame
-    #     new_df = df
-    #     new_df[col_name_results] = my_list
-    # output
-    # return new_df
+    """
+    Args:
+        df (): general dataframe.
+        col_name_results (): the column name to which the data will be aggregated.
 
-
-if __name__ == "__main__":
-    '''
-        x = pd.DataFrame(data={'col1': [1, 2, 3, 4, 5], 'col2': [3, 4, 5, 6, 7]})
-        col_series = x['col1']
-        filtered_indexes = col_series[(col_series >= 2) & (col_series < 4)]
-        new_df = x[list(filtered_indexes.index)[0]: list(filtered_indexes.index)[1] + 1:]
-    '''
-
-    final = double_special_letters({},"letter" ,"value", "result")
-    print(final)
+    Returns:
+        The function creates a new column.
+         Each value in this column contains a list of all the items in the row.
+    """
+    df[col_name_results] = df.agg(list, axis=1)
+    return df
--- course_6130/EX5/ex5b.py
+++ course_6130/EX5/ex5b.py
@@ -1,10 +1,8 @@
 # 123456789, 123456789
-# time
-import glob
+# 15
 
-import numpy as np
+import glob
 import pandas as pd
-import os
 
 PATH = r'C:\Users\asafk\Desktop\Hebrow Univarcity\First Year\Semester B\Python Cognitive Science\EX5\friends_datasets_ex5'
 
@@ -12,10 +10,65 @@ def get_file_name_list_in_folder(folder_path):
     files_names = glob.glob(folder_path + r'\friends_imdb_episode_season_*')
     return list(files_names)
 
-
 # question 2
 def create_season_rating(csv_folder):
+    """
+    Args:
+        csv_folder: folder containing csv files
 
+    Returns: DF with all the seasons rated (numVotes = num of episodes, sumvotes\avrargeVotes per episode)
+    """
+    list_files_names = get_file_name_list_in_folder(csv_folder)
+    df_all_episodes = pd.read_csv(list_files_names[0])
+
+    # reading all episodes data
+    for index in range(1, len(list_files_names)):
+        curr_season_df = pd.read_csv(list_files_names[index])
+        df_all_episodes = pd.concat([df_all_episodes, curr_season_df], ignore_index=True, axis=0)
+
+    # reading imdb data
+    df_imdb = pd.read_csv(csv_folder + r'\rating_all_imdb.csv')
+
+    # merging the data frames
+    df_merged = df_all_episodes.merge(df_imdb, how="inner", on=["tconst"])
+
+    # creating a Weighted average
+    df_merged['sum_Rating'] = df_merged.apply(lambda e: e['averageRating'] * e['numVotes'], axis=1)
+    seasons_sum = df_merged.groupby(by="seasonNumber", as_index=False).sum()[['seasonNumber', 'averageRating', 'numVotes','sum_Rating' ]]
+    seasons_sum['Weighted_averageRating'] = df_merged.apply(lambda e: e['sum_Rating'] / e['numVotes'], axis=1)
+    seasons_sum.rename(columns={'numVotes': 'sumNumVotes'},inplace = True)
+
+    # deep coping the information
+    seasons_temp = df_merged.copy(deep = True)
+
+    # cleaning the data - deleting the pointer
+    seasons_temp.drop('tconst', inplace=True, axis=1)
+    seasons_rating = seasons_temp.groupby(by="seasonNumber", as_index=False).mean()[['seasonNumber' , 'numVotes']]
+    seasons_rating['Weighted_averageRating'] = seasons_sum['Weighted_averageRating']   ###
+    ### to change the averageRating to regular, write 'averageRating' instead of 'Weighted_averageRating' on both sides!
+    seasons_rating.rename(columns={'numVotes': 'averageNumVotes'}, inplace = True)
+
+    # seasons_rating['numVotes'] = seasons_rating.apply(lambda r: r['sumNumVotes'] / r['averageNumVotes'], axis=1) # if isinstance(r['sumNumVotes'], (int or float)) else r['sumNumVotes'] / r['averageNumVotes']
+    seasons_rating['sumNumVotes'] = seasons_sum['sumNumVotes']
+    seasons_rating['numVotes'] = seasons_rating.apply(lambda r: r['sumNumVotes'] / r['averageNumVotes'], axis = 1)
+    seasons_rating.drop(['averageNumVotes','sumNumVotes'], inplace=True, axis=1)
+
+    final_df = seasons_rating
+
+    return (final_df)
+
+
+# question 3
+def most_lines_in_the_second_most_popular_episode(csv_folder):
+    """
+
+    Args:
+        csv_folder: folder containing csv files
+
+    Returns: DF containing the num of quotes each character has in  the second most popular ep. (based on numvotes)
+
+    """
+    # creating the base DF from the folder
     list_files_names = get_file_name_list_in_folder(csv_folder)
     df_all_episodes = pd.read_csv(list_files_names[0])
 
@@ -25,23 +78,58 @@ def create_season_rating(csv_folder):
 
     df_imdb = pd.read_csv(csv_folder + r'\rating_all_imdb.csv')
     df_merged = df_all_episodes.merge(df_imdb, how="inner", on=["tconst"])
-    print(list(df_merged.shape))
-    print(list(df_merged.columns))
+    df_lines = pd.read_csv(csv_folder + r'\friends_quotes.csv')
 
-    seasons_sum = df_merged.groupby(by="seasonNumber", as_index=False).sum()[['seasonNumber', 'averageRating', 'numVotes']]
-    seasons_rating = df_merged.groupby(by="seasonNumber", as_index=False).mean()[['seasonNumber', 'averageRating']]
+    # sort the values in order to extract the relevant episode
+    df_sorted_ep = df_merged.sort_values(by='numVotes', ascending=False)
+    df_sort_ep_new_index = df_sorted_ep.reset_index()
+    df_new = df_sort_ep_new_index.rename(columns={'episodeNumber': 'episode_number', 'seasonNumber': 'season'})
+    rel_ep_num = df_new.loc[1, 'episode_number']
+    rel_seson_num = df_new.loc[1, 'season']
 
-    final_df = seasons_rating
-    final_df["numVotes"] = seasons_sum["numVotes"]
+    # unite the base DF with the quotes
+    df_work1 = df_new[df_new['episode_number'] == rel_ep_num]
+    df_work2 = df_work1[df_work1['season'] == rel_seson_num]
+    df_relevant_ep = df_work2.merge(df_lines, how="inner", on=["season","episode_number"], )
 
-    return (final_df)
+    # group by charecters
+    df_final = pd.DataFrame(df_relevant_ep.groupby(by=["author"], as_index=False))
+    df_final['number_of_lines'] = pd.DataFrame(df_relevant_ep.groupby(by=["author"], as_index=False).count()['quote_order'])
+    df_final = df_final.sort_values(by = 'number_of_lines',ascending=False )
+    df_final = df_final.reset_index()
 
-def most_lines_in_the_second_most_popular_episode(csv_folder):
-    df_line = pd.read_csv(csv_folder + r'friends_quotes.csv')
+    # orgenazing the data
+    df_final['episode_number'] = rel_ep_num
+    df_final['season_number'] = rel_seson_num
+    df_final = df_final.drop(columns = [1,'index'])
+    df_final.rename(columns={0: 'character_name'} ,inplace = True )
+    df_final = pd.DataFrame(df_final)
+    return df_final
 
 def who_said_it(csv_folder, character_name, phrase):
-    pass
+    """
+    Args:
+        csv_folder: containing csv files
+        character_name:
+        phrase: sentence or a word
+
+    Returns: how many times this character uses the phrase
+    """
+
+    # creating data Frame and parameters
+    rel_phrase = phrase.lower()
+    rel_char = character_name.lower()
+    df_lines = pd.read_csv('\\friends_quotes.csv')
+    df_lines_per_char = df_lines
+    df_lines_per_char['quote'] = df_lines_per_char.apply(lambda l: l['quote'].lower(), axis=1)
+    df_lines_per_char['author'] = df_lines_per_char.apply(lambda l: l['author'].lower(), axis=1)
+
+    # filtering to the relevant DF
+    df_chosen_char = df_lines_per_char[df_lines_per_char['author'] == rel_char]
+    df_final = df_chosen_char[df_chosen_char['quote'].str.contains(rel_phrase)]
+    num_of_phrase = df_final.shape[0]
+
+    return num_of_phrase
 
 
-if __name__ == "__main__":
-    create_season_rating(PATH)
+
